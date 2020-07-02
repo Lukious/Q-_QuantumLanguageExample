a4eaf84c74ac9e81b793a1d1c2115f4aadbe4dfc
lukious <zhsjzhsj@gmail.com>
2020-07-02 12:18:11 +0900

    Init commit of turorials

---
+++ Tutorial/RandNumGen/RandNumGen.py
@@ -0,0 +1,7 @@
+import qsharp
+
+from Qrng import SampleQuantumRandomNumberGenerator
+
+Num = SampleQuantumRandomNumberGenerator.simulate()
+
+print(Num)
---
+++ Tutorial/SimpleGrove/RandNumGen.py
@@ -0,0 +1,5 @@
+import qsharp
+import Microsoft.Quantum.Samples.SimpleGrover
+from Microsoft.Quantum.Samples.SimpleGrover import SearchForMarkedInput
+
+SearchForMarkedInput.simulate()
